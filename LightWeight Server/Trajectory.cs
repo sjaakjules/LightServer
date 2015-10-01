7715cf6ad82a53abe15280019e6241958ffe54f5
juju <julianrutten@outlook.com>
2015-10-01 13:08:57 +1000

    added quintic solutions

--- LightWeight Server/Trajectory.cs
+++ LightWeight Server/Trajectory.cs
@@ -157,6 +157,49 @@ namespace LightWeight_Server
             return new Pose(changeQ, new Vector3(translationComponent.X, translationComponent.Y, translationComponent.Z));
 
         }
+
+        /// <summary>
+        /// Constructs a quintic with position and velocity specified halfway in the trajectory
+        /// </summary>
+        /// <param name="x0"></param> Start Position
+        /// <param name="xf"></param> Final Position
+        /// <param name="xm"></param> Mid Position
+        /// <param name="x0d"></param> start Velocity
+        /// <param name="xfd"></param> final velocity
+        /// <param name="xmd"></param> mid velcoity
+        /// <param name="tf"></param> time for trajectory
+        /// <returns></returns>
+        double[] Quintic(double x0, double xf, double xm, double x0d, double xfd, double xmd, double tf)
+        {
+            double a0 = x0;
+            double a1 = x0d;
+            double a2 = (7 * xf - 23 * x0 + 16 * xm) / Math.Pow(tf, 2) - (6 * x0d + xfd + 8 * xmd) / tf;
+            double a3 = (13 * x0d + 5 * xfd + 32 * xmd) / Math.Pow(tf, 2) - (34 * xf - 66 * x0 + 32 * xm) / Math.Pow(tf, 3);
+            double a4 = (52 * xf - 68 * x0 + 16 * xm) / Math.Pow(tf, 4) - (12 * x0d + 8 * xfd + 40 * xmd) / Math.Pow(tf, 3);
+            double a5 = (4 * x0d + 4 * xfd + 16 * xmd) / Math.Pow(tf, 4) + (24 * x0 - 24 * xf) / Math.Pow(tf, 5);
+            return new double[] { a0, a1, a2, a3, a4, a5 };
+        }
+
+        /// <summary>
+        /// Constructs a quintic with position specified halfway in the trajectory where there is zero acceleration
+        /// </summary>
+        /// <param name="x0"></param>
+        /// <param name="xf"></param>
+        /// <param name="xm"></param>
+        /// <param name="x0d"></param>
+        /// <param name="xfd"></param>
+        /// <param name="tf"></param>
+        /// <returns></returns>
+        double[] Quintic(double x0, double xf, double xm, double x0d, double xfd, double tf)
+        {
+            double a0 = x0;
+            double a1 = x0d;
+            double a2 = -(16 * x0 + 16 * xf - 32 * xm) / Math.Pow(tf, 2) - (5 * x0d - 5 * xfd) / tf;
+            double a3 = (38 * x0 + 58 * xf - 96 * xm) / Math.Pow(tf, 3) + (9 * x0d - 19 * xfd) / Math.Pow(tf, 2);
+            double a4 = -(33 * x0 + 63 * xf - 96 * xm) / Math.Pow(tf, 4) - (7 * x0d - 22 * xfd) / Math.Pow(tf, 3);
+            double a5 = (10 * x0 + 22 * xf - 32 * xm) / Math.Pow(tf, 5) + (2 * x0d - 8 * xfd) / Math.Pow(tf, 4);
+            return new double[] { a0, a1, a2, a3, a4, a5 };
+        }
     }
 }
 
